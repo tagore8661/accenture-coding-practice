4ffab8a425270fc580d88b0cda43738dcc6e6f8d
GANUGA TAGORE <tagore8661@gmail.com>
2025-05-20 00:16:51 +0530

    Problem-90

---
+++ Problem-90.py
@@ -0,0 +1,15 @@
+#Write a program to check if the given number is palindrome or not.
+
+num = int(input("Enter a number: "))
+# Input: 12321
+temp = num
+reverse = 0
+while temp > 0:
+    remainder = temp % 10
+    reverse = (reverse * 10) + remainder
+    temp = temp // 10
+if num == reverse:
+  print('Palindrome')
+else:
+  print("Not Palindrome")
+# Output: Palindrome
