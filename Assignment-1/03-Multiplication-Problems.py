ea5320e0341beb40f4c6dfd4c9d1101ef0637f55
GANUGA TAGORE <tagore8661@gmail.com>
2025-05-20 01:32:38 +0530

    Multiplication Problems

---
+++ Assignment-1/03-Multiplication-Problems.py
@@ -0,0 +1,26 @@
+"""
+1. Product of Two Numbers -
+
+○ Write a program to take two integers a and b as input and calculate their product.
+○ Expression: product = a * b
+
+2. Product of Three Numbers -
+
+○ Write a program to take three integers a, b, and c as input and calculate their product.
+○ Expression: product = a * b * c
+
+"""
+
+#1. Product of Two Numbers
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+product_two = a * b
+print("Product of two numbers:", product_two)
+
+
+#2. Product of Three Numbers
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+c = int(input("Enter third number: "))
+product_three = a * b * c
+print("Product of three numbers:", product_three)
