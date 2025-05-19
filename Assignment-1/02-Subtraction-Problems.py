4e705b857bb6e5d2685ae758385a97a0b77728fb
GANUGA TAGORE <tagore8661@gmail.com>
2025-05-20 01:31:23 +0530

    Subtraction Problems

---
+++ Assignment-1/02-Subtraction-Problems.py
@@ -0,0 +1,26 @@
+"""
+1. Difference of Two Numbers -
+
+○ Write a program to take two integers a and b as input and calculate their difference (a - b).
+○ Expression: difference = a - b
+
+2. Difference of Three Numbers -
+
+○ Write a program to take three integers a, b, and c as input and calculate the result of subtracting b and c from a.
+○ Expression: result = a - b - c
+
+"""
+
+#1. Difference of Two Numbers
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+difference = a - b
+print("Difference of two numbers:", difference)
+
+
+#2. Difference of Three Numbers
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+c = int(input("Enter third number: "))
+difference_three = a - b - c
+print("Difference of three numbers:", difference_three)
