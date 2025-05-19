2cd8579bc2ea167e9a4a033288ca71be9ef59bf6
GANUGA TAGORE <tagore8661@gmail.com>
2025-05-20 00:14:54 +0530

    Problem-77

---
+++ Problem-77.py
@@ -0,0 +1,12 @@
+#floyds triangle
+
+def print_pattern(n):
+    num = 1
+    for i in range(1, n + 1):
+        for _ in range(i):
+            print(num, end=" ")
+            num += 1
+        print()
+
+n = int(input())
+print_pattern(n)
