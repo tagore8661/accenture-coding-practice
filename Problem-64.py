4719d10daee0aba27fdcb1943ce24169025afeb8
GANUGA TAGORE <tagore8661@gmail.com>
2025-05-20 00:12:44 +0530

    Problem-64

---
+++ Problem-64.py
@@ -0,0 +1,11 @@
+#Intersection of Two Arrays
+
+def intersection(arr1, arr2):
+    result = []
+    for num in arr1:
+        if num in arr2 and num not in result:
+            result.append(num)
+    return result
+
+# Test
+print(intersection([1, 2, 2, 3, 4], [2, 2, 3, 5]))  # Output: [2, 3]
