7c31970e5c7b644c9d6a2943590fd0bf272ce43d
GANUGA TAGORE <tagore8661@gmail.com>
2025-05-20 01:30:48 +0530

    Addition Problems

---
+++ Assignment-1/01-Addition-Problems.py
@@ -0,0 +1,25 @@
+"""
+1. Sum of Two Numbers -
+
+○ Write a program to take two integers a and b as input and calculate their sum.
+○ Expression: sum = a + b
+
+2. Sum of Three Numbers -
+
+○ Write a program to take three integers a, b, and c as input and calculate their sum.
+○ Expression: sum = a + b + c
+"""
+
+#1. Sum of Two Numbers
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+sum_two = a + b
+print("Sum of two numbers:", sum_two)
+
+
+#2. Sum of Three Numbers
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+c = int(input("Enter third number: "))
+sum_three = a + b + c
+print("Sum of three numbers:", sum_three)
